8dae8c8aaffee0c7f47e6fda3484ccb67a387833
Sean Chen <seanchen47@gmail.com>
2016-09-25 15:48:58 +0800

    user profile

--- Richmond/index/views.py
+++ Richmond/index/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def index_view(request):
-	return render(request, 'index.html')
+	return render(request, 'index.html', {
+		'user': request.user
+	})
--- Richmond/players/views.py
+++ Richmond/players/views.py
@@ -3,6 +3,7 @@ from django.contrib import auth
 from django.contrib.auth.models import User
 from django.core.context_processors import csrf
 from django.contrib.auth.forms import UserCreationForm
+from players.models import Profile
 
 def login_view(request):
 	return render(request, 'account/login.html')
@@ -32,6 +33,8 @@ def register(request):
 			password = request.POST['password1']
 			user = User.objects.create_user(username, email, password)
 			user.save()
+			user_profile = Profile(user = user)
+			user_profile.save()
 			# perhaps set permissions of the new user
 			return redirect('/accounts/login', permanent = True)
 	else:
